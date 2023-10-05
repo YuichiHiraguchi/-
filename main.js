33dfa35c51a13122f93eddcfb3a99886b0334eca
YuichiHiraguchi <64597373+YuichiHiraguchi@users.noreply.github.com>
2023-10-05 16:07:29 +0900

    Update main.js

--- main.js
+++ main.js
@@ -19,8 +19,8 @@
 
   var func2=function(){
     if(jikan.textContent!=="終了"){
-      document.getElementById('nyuryoku').blur();
-      document.getElementById('nyuryoku').focus();
+   //   document.getElementById('nyuryoku').blur();
+   //   document.getElementById('nyuryoku').focus();
         do {
           n = Math.floor(Math.random()*results.length);
         } while (n==nn || n==nn+20 || n==nn-20);
