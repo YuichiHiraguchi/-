8f61c3529ee1657917a01b50e7bed1d34a1045ac
YuichiHiraguchi <64597373+YuichiHiraguchi@users.noreply.github.com>
2023-10-30 09:39:42 +0900

    Update main.js

--- main.js
+++ main.js
@@ -71,8 +71,8 @@
     }
 
     function timer() {
-      const minnyuryoku=hankaku2zenkaku(document.getElementById('hun')).value;
-      const secnyuryoku=hankaku2zenkaku(document.getElementById('byo')).value;
+      const minnyuryoku=hankaku2Zenkaku(document.getElementById('hun')).value;
+      const secnyuryoku=hankaku2Zenkaku(document.getElementById('byo')).value;
       sec=sec+1;
       let min1=Math.floor(sec/60);
       let sec1=sec-min1*60;
