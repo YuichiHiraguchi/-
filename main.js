73abde2e1286fcfb25f2601f10e90e4333a60543
YuichiHiraguchi <64597373+YuichiHiraguchi@users.noreply.github.com>
2023-10-30 10:30:07 +0900

    Update main.js

--- main.js
+++ main.js
@@ -73,6 +73,7 @@ function hankaku2Zenkaku(str) {
               return str.replace(/[Ａ-Ｚａ-ｚ０-９！＂＃＄％＆＇（）＊＋，－．／：；＜＝＞？＠［＼］＾＿｀｛｜｝]/g, function(s) {
                   return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
               });
+}
   
     function timer() {
       const minnyuryoku=hankaku2Zenkaku(document.getElementById('hun').value);
