68b23f129cf0f5d2969b073b65f660ac3504c8c8
YuichiHiraguchi <64597373+YuichiHiraguchi@users.noreply.github.com>
2023-10-30 10:38:21 +0900

    Update main.js

--- main.js
+++ main.js
@@ -100,11 +100,6 @@ function hankaku2Zenkaku(str) {
         }
       }
             };
-            function hankaku2Zenkaku(str) {
-              return str.replace(/[Ａ-Ｚａ-ｚ０-９！＂＃＄％＆＇（）＊＋，－．／：；＜＝＞？＠［＼］＾＿｀｛｜｝]/g, function(s) {
-                  return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
-              });
-          }
 
   btn1.addEventListener('click',()=>{
     return func2();
